65fa902ab87bdd1e1986101e704d1f87a3715e5a
gabrielrial <gabrielrialpicco@gmail.com>
2026-06-28 11:28:23 +0200

    feat: class post added

---
+++ app/database/models/post.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Text
+from sqlalchemy.orm import relationship
+from app.database.conf.alch_conf import Base
+from sqlalchemy import func
+
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+
+    author = relationship("User", back_populates="posts")
+    author_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+
+    content = Column(Text, nullable=False)
+
+    created_at = Column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
+
+    comments = relationship("Comment", backref="post", cascade="all, delete-orphan")
