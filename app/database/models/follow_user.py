9dfa9ea3304c55e8a8ab50f4a8cc3eac79c3a9ee
gabrielrial <gabrielrialpicco@gmail.com>
2026-09-26 00:34:08 +0200

    feature: followuser model implemented

---
+++ app/database/models/follow_user.py
@@ -0,0 +1,15 @@
+from app.database.conf.alch_conf import Base
+from sqlalchemy import Column, Integer, DateTime,ForeignKey, func
+from sqlalchemy.orm import relationship
+from app.database.models.user import User
+
+
+class FollowUser(Base):
+    __tablename__ = "userfollows"
+
+    follower_id = Column(Integer, ForeignKey("users.id"), primary_key=True)
+    following_id = Column(Integer, ForeignKey("users.id"), primary_key=True, index=True)
+
+    created_at = Column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
