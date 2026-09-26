9fb5748205a90d694123475b8cb8849d19ac73b4
gabrielrial <gabrielrialpicco@gmail.com>
2026-09-26 09:58:47 +0200

    feature: get_user_or_404() implemented in services user

--- app/services/users.py
+++ app/services/users.py
@@ -6,3 +6,9 @@ def get_user_by_email(db: Session, email: str):
     
 def get_user_by_username(db: Session, username: str):
     return db.query(User).filter(User.username == username).first()
+
+def get_user_or_404(db: Session, user_id: int) -> User:
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= "Not found")
+    return user
